3a927201f700fcd28b719a036830c41fc9be02a6
Michael F. Herbst <info@michael-herbst.com>
2016-12-13 20:58:02 +0100

    Updated versions of the scripts to find optimal k and plot k vs E

--- scripts/common.py
+++ scripts/common.py
@@ -3,19 +3,67 @@ import re
 
 binary="../build/examples/hf/hf"
 
-def getE(params):
-    res = subprocess.check_output(
-        [binary,"--Z", str(params["Z"]), 
-         "--alpha", str(params["alpha"]), 
-         "--beta", str(params["beta"]), 
-         "--nmax", str(params["nmax"]), 
-         "--kexp", str(params["k"])]
-    ).splitlines()
+def molsturm(params):
+    plist = [binary]
+    for (k,v) in params.items():
+        plist.append("--" + k)
+        plist.append(str(v))
+
+    res = subprocess.check_output(plist).splitlines()
+
+    # Result dictionary
+    out = {}
 
     expr = re.compile("E_total *= *(-?[0-9.eE]+)")
     for line in res:
         foundit = expr.search(str(line))
         if foundit:
-            return float(foundit.group(1))
+            out["hf_energy"] = float(foundit.group(1))
+            return out
     raise SystemExit("something wrong here")
+
+def add_args_to_parser(parser):
+    """
+    Add the arguments understood by molsturm to the parser
+    """
+    parser.add_argument("--Z_charge",metavar="Z",type=float,default=None,help="Nuclear charge")
+    parser.add_argument("--alpha",metavar="na",type=int,default=None,help="Number of alpha electrons")
+    parser.add_argument("--beta",metavar="nb",type=int,default=None,help="Number of beta electrons")
+
+    parser.add_argument("--error",metavar="tol", type=float,default=1e-4,help="Error for the computation (Default: 1e-4)")
+    parser.add_argument("--max_iter",metavar="iter", type=float,default=100,help="Maximal number of iterations (Default: 100)")
+
+    parser.add_argument("--n_max",metavar="n", type=int,default=3,help="Maximal number for n")
+    parser.add_argument("--l_max",metavar="l", type=int,default=None,help="Maximal number for l (Default: n-1)")
+
+def get_params(args):
+    """
+    Get the params for system, basis and convergence
+    """
+    params = {}
+
+    # Basis Set params
+    params["basis_type"] = "cs_naive"
+    params["n_max"] = args.n_max
+
+    if args.l_max is None:
+        params["l_max"] = args.n_max -1
+        args.l_max = args.n_max -1
+    else:
+        params["l_max"] = args.l_max
+
+    if args.Z_charge is None or args.alpha is None or args.beta is None:
+        raise SystemExit("Script needs --Z_charge, --alpha, --beta")
+
+    # System params
+    params["Z_charge"] = args.Z_charge
+    params["alpha"] = args.alpha
+    params["beta"] = args.beta
+
+    # Convergence params
+    params["n_eigenpairs"] = 100  #TODO To make molsturm converge
+    params["error"] = args.error
+    params["max_iter"] = args.max_iter
+
+    return params
 
--- scripts/find_minimal_k.py
+++ scripts/find_minimal_k.py
@@ -1,47 +1,78 @@
 #!/usr/bin/env python3
 
 import common
+import math
+from scipy.optimize import minimize
+import argparse
 
-params = {
-    "Z": 4, 
-    "alpha": 2,
-    "beta": 2,
-    "nmax": 3,
-}
+def add_args_to_parser(parser):
+    """Add all required variables to the parser"""
+    common.add_args_to_parser(parser)
+    parser.add_argument("--kerror",metavar="tol",type=float,default=1e-6,help="Maximal remaining error in k (Default: 1e-6)")
+    parser.add_argument("--kguess",metavar="k", type=float,default=None,help="A guess for k (taken as sqrt(Z) if absent)")
 
-# search space
-kmin=0.5
-kmax=2.4
+def parse_args(parser):
+    """
+    Parse all args from the parser object required for this module
+    Return the args parsed, raise SystemExit on any error.
+    """
+    args = parser.parse_args()
 
-tolk = 1e-8
+    if args.kguess is None:
+        if args.Z_charge is None:
+            args.kguess = 1.
+        else:
+            args.kguess = math.sqrt(args.Z_charge)
 
-print("This is not the best algorithm and does not always converge to the actual minimum")
+    return args
+
+def find_minimal_k(params,kguess,kerror,disp=True):
+    """
+    Find the minimal k for the system and basis set
+    described by param.
+    Start from kguess and optimise until an error threshold
+    below kerror is reached.
+
+    disp:  Control whether to display convergence progress
+    """
+    # The energy functional to optimise
+    def energy(args):
+        params["kexp"] = args[0]
+        return common.molsturm(params)["hf_energy"]
+
+    if disp:
+        # Setup printing of convergence:
+
+        print("   iter    kexp")
+        def priter(args):
+            print("   {0:4d}    {1}".format(priter.count,args[0]))
+            priter.count += 1
+        priter.count=0
+        priter([kguess])
+    else:
+        priter=None
+
+    # Minimise the functional wrt. k
+    res = minimize(energy,kguess,tol=args.kerror,options={"disp": disp},callback=priter)
+
+    if not res.success:
+        raise SystemExit("Error converging, got up to k="+str(res.x))
+
+    return res.x[0]
 
 #
 # --------------------------------------------
 #
 
-def getE(k):
-    print("Running with k =",k)
-    params["k"] = k
-    E = common.getE(params)
-    print("    E =", E)
+if __name__ == "__main__":
+    # Parse arguments:
+    parser = argparse.ArgumentParser("Minimise the k exponent of a coulomb sturmian basis for a system.")
+    add_args_to_parser(parser)
+    args = parse_args(parser)
+    params = common.get_params(args)
 
-Ekmin = getE(kmin)
-Ekmax = getE(kmax)
+    kmin = find_minimal_k(params,args.kguess,args.kerror,disp=True)
 
-while (abs(kmin-kmax) > tolk):
-    kcurr= (kmin+kmax)/2
-    Ecurr = getE(kcurr)
-    if Ecurr < Ekmin:
-        Ekmin = Ecurr
-        kmin = kcurr
-    elif Ecurr < Ekmax:
-        Ekmax = Ecurr
-        kmax = kcurr
-    else:
-        raise SystemExit("something wrong")
-
-kcurr= (kmin+kmax)/2
-Ecurr = getE(kcurr)
-print("k: ", kcurr,"    E:", Ecurr)
+    # Print results: n=Number of digits to print
+    n = int(-math.log(args.kerror)/math.log(10)+0.5)
+    print(("kopt = {0:."+str(n)+"f}  (tol={1})").format(kmin,args.kerror))
