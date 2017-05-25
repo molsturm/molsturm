3e1e6e914104f8bde3889a5facf4f1c94d5126ab
Michael F. Herbst <info@michael-herbst.com>
2017-05-25 12:15:48 +0200

    Tests for mp2 energies

--- tests/molsturm_iface_tests/test_Energies.py
+++ tests/molsturm_iface_tests/test_Energies.py
@@ -23,26 +23,41 @@
 
 from NumCompTestCase import NumCompTestCase
 import molsturm
-import data_hf_energies as data
+import molsturm.posthf
 import numpy as np
+import unittest
 
-class TestHartreeFockEnergies(NumCompTestCase):
+import data_energies as data
+
+class TestEnergies(NumCompTestCase):
   """This test should ensure, that we our molsturm can reproduce
      data exactly in the way computed with ORCA or another standard
      quantum chemistry program.
   """
 
-  def run_case(self, case):
-    inparams = dict(data.params_all)
-    inparams.update(case["system"])
-    inparams.update(case["params"])
+  @classmethod
+  def import_cases(cls):
+    # Parse input and build test cases:
+    return data.cases
 
-    hf = molsturm.hartree_fock(**inparams)
+  @classmethod
+  def setUpClass(cls):
+    cls.cases = cls.import_cases()
+    cls.hf_results = dict()
+
+  def run_hf(self,case):
+    error = case["params"]["error"]
+
+    params = dict(case["params"])
+    params.update({
+      "eigensolver":    "lapack",
+      "guess_esolver":  "lapack",
+    })
+
+    hf = molsturm.hartree_fock(**params)
     hf_1e = hf["energy_kinetic"] + hf["energy_nuclear_attraction"]
     hf_2e = hf["energy_coulomb"] + hf["energy_exchange"]
-
-    error = inparams["error"]
-    ref = case["reference"]
+    ref = case["reference_hf"]
 
     self.assertAlmostEqual(hf_1e, ref["energy_1e"], tol=10*error, prefix="1e energy: ")
     self.assertAlmostEqual(hf_2e, ref["energy_2e"], tol=10*error, prefix="2e energy: ")
@@ -55,9 +70,34 @@ class TestHartreeFockEnergies(NumCompTestCase):
       tol=max(1e-6, error)   # Unfortunately ORCA spits out the mos only up to very low accuracy
       mos = np.array(ref["energy_mos"])
       self.assertArrayAlmostEqual(hf["orbital_energies_f"], mos, tol=tol, prefix="MO energies: ")
+    return hf
 
-  def test_run_all_cases(self):
-    for case in data.cases:
-      with self.subTest(msg=case["description"]):
-        self.run_case(case)
+  def run_mp2(self,case,hf):
+    if not "reference_mp2" in case: return None
+
+    error = case["params"]["error"]
+
+    mp2 = molsturm.posthf.mp2(hf, max_memory=64*1024*1024)
+    mp2_ref = case["reference_mp2"]
+    self.assertAlmostEqual(mp2["energy_mp2"], mp2_ref["energy_mp2"],
+                           tol=error, prefix="MP2 energy: ")
+    self.assertAlmostEqual(mp2["energy_ground_state"], mp2_ref["energy_ground_state"],
+                           tol=error, prefix="Ground state energy: ")
+
+  # --------------------------------------------
+
+  def test_0_hf(self):
+    for caselal in self.cases:
+      with self.subTest(label=caselal):
+        self.hf_results[caselal] = self.run_hf(self.cases[caselal])
+
+  @unittest.skipUnless("mp2" in molsturm.posthf.available_methods,
+                      "mp2 not available => Skipping mp2 tests")
+  def test_1_mp2(self):
+    for caselal in self.cases:
+      with self.subTest(label=caselal):
+        if not caselal in self.hf_results:
+          raise self.fail("HF results not available")
+        else:
+          self.run_mp2(self.cases[caselal],self.hf_results[caselal])
 
