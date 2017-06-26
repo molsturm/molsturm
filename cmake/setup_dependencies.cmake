358edeb24735b5c1475673fdcc3bce06023e70d6
Michael F. Herbst <info@michael-herbst.com>
2017-06-26 17:34:17 +0200

    Remove sturmint as submodule (pulled in via gint now)

--- cmake/setup_dependencies.cmake
+++ cmake/setup_dependencies.cmake
@@ -1,14 +1,14 @@
 # sets these things
 #
-# 	MOLSTURM_DEPENDENCIES			everyone needs these libraries
-# 	MOLSTURM_DEPENDENCIES_DEBUG		debug mode needs these extras
-# 	MOLSTURM_DEPENDENCIES_RELEASE		release mode needs these extras
-# 	MOLSTURM_DEPENDENCIES_TEST		tests need these extra libraries
+#       MOLSTURM_DEPENDENCIES			everyone needs these libraries
+#       MOLSTURM_DEPENDENCIES_DEBUG		debug mode needs these extras
+#       MOLSTURM_DEPENDENCIES_RELEASE		release mode needs these extras
+#       MOLSTURM_DEPENDENCIES_TEST		tests need these extra libraries
 #
 #       MOLSTURM_DEFINITIONS			definitions for all compilation
 #       MOLSTURM_DEFINITIONS_DEBUG		definitions for debug mode
 #       MOLSTURM_DEFINITIONS_RELEASE		definitions for release mode
-#       
+#
 
 ####################
 #-- Empty it all --#
@@ -80,6 +80,5 @@ endif()
 
 add_submodule_dependency(krims 0.0.0)
 add_submodule_dependency(linalgwrap 0.2.0)
-add_submodule_dependency(sturmint 0.0.0)
 add_submodule_dependency(gint 0.0.0)
 add_submodule_dependency(gscf 0.0.0)
