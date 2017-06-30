aabae611bda96d0d87e089db6f1b5a5df83c3493
Michael F. Herbst <info@michael-herbst.com>
2017-06-30 11:23:46 +0200

    Allow to use (some) previous calculations as a guess

--- src/molsturm_iface/molsturm/_hartree_fock.py
+++ src/molsturm_iface/molsturm/_hartree_fock.py
@@ -25,6 +25,7 @@ import molsturm_iface as iface
 import numpy as np
 from ._constants import HFRES_ARRAY_KEYS, HFRES_INPUT_PARAMETER_KEY
 from collections import Iterable
+from .scf_guess import extrapolate_from_previous
 
 def __to_double_vector(val):
   ret = iface.DoubleVector()
@@ -88,69 +89,143 @@ def __to_atoms(li):
     raise ValueError("atoms needs to be a string or a list of strings")
   return ret
 
+
+def __setup_user_provided_guess(kwargs, inputargs):
+  """
+  This is triggered if guess_external_orben_f or guess_external_orbcoeff_bf
+  is provided by the user. This may only be done if "guess" is also set
+  to external. Alternatively we set guess to external and check that both
+  values are provided.
+  """
+  if not "guess" in kwargs or kwargs["guess"] != "external":
+    raise ValueError("The parameters 'guess_external_orben_f' or "
+                     "'guess_external_orbcoeff_bf' may only be provided iff 'guess'"
+                     " is 'external'")
+
+  if not "guess_external_orben_f" in kwargs \
+     or not "guess_external_orbcoeff_bf" in kwargs:
+    raise ValueError("If the guess parameter is set to 'external', the parameters "
+                     "'guess_external_orben_f' (for the guess orbital energies) and "
+                     "'guess_external_orbcoeff_bf' (and the guess orthonormal SCF "
+                     "coefficients) are both required.")
+
+  inputargs["guess_external_orben_f"] = kwargs["guess_external_orben_f"]
+  inputargs["guess_external_orbcoeff_bf"] = kwargs["guess_external_orbcoeff_bf"]
+  inputargs["guess"] = "external"
+
+
+def __setup_guess(kwargs, inputargs):
+  """
+  This is triggered if guess is provided by the user.
+  """
+  guess_key = "guess"
+  guess = kwargs[guess_key]
+
+  if isinstance(guess, str):
+    if val == "external":
+      __setup_user_provided_guess(kwargs, inputargs)
+    else:
+      inputargs[guess_key] = guess
+    return
+  elif not isinstance(guess, dict):
+    raise TypeError("The value provided to guess must be a previous hartree_fock result "+
+                    " or a string describing a valid guess method.")
+
+  inputargs["guess_external_orben_f"], inputargs["guess_external_orbcoeff_bf"] = \
+      extrapolate_from_previous(guess, kwargs)
+  inputargs["guess"] = "external"
+
+
+# Map from input kwargs to inputargs list
+__kwargs_parse_map = {
+  "guess":                       __setup_guess,
+  "guess_external_orben_f":      __setup_user_provided_guess,
+  "guess_external_orbcoeff_bf":  __setup_user_provided_guess,
+}
+
 # Special input parameters for which the above conversion functions need
 # to be used before assignment
-__params_transform_maps = {
-  "coords":       __to_coords,
-  "atom_numbers": __to_atom_numbers,
-  "atoms":        __to_atoms,
-  "nlm_basis":    __to_nlm,
+__params_transform_map = {
+  "coords":                      __to_coords,
+  "atom_numbers":                __to_atom_numbers,
+  "atoms":                       __to_atoms,
+  "nlm_basis":                   __to_nlm,
+  "guess_external_orben_f":      __to_double_vector,
+  "guess_external_orbcoeff_bf":  __to_double_vector,
 }
+
+
+# Extra keys witch which we deal with on the python level
+HF_EXTRA_KEYS = [ ]
+
 
 # These keys exist in the Parameters struct, but should not be exposed
 # via the hartree_fock interface
-HF_PARAMS_EXCLUDE_KEYS = [ "restricted_set_by_user", "all" ]
+HF_PARAMS_EXCLUDE_KEYS = [ "all" ] + [ k for k in dir(iface.Parameters)
+                                       if k[0] == "_" or k.startswith("internal_") ]
 
 """The list of keys understood by the hartree_fock function"""
-hartree_fock_keys = [ k for k in dir(iface.Parameters)
-                      if k[0] != "_" and not k in HF_PARAMS_EXCLUDE_KEYS ]
+hartree_fock_keys = HF_EXTRA_KEYS + [ k for k in dir(iface.Parameters)
+                                      if not k in HF_PARAMS_EXCLUDE_KEYS ]
 
-def hartree_fock(forward_parameters=True, **kwargs):
+def hartree_fock(**kwargs):
   """
   Run a Hartree-Fock calculation with molsturm. The list of valid input
   parameters can be retrieved by the means of the list "hartree_fock_keys".
-
-  Parameters:
-    forward_parameters:   Should the list of kwargs be forwarded to the
-                          output dictionary for archiving purposes?
-                          If True(default) the returned dictionary
-                          will contain a key "input_parameters" which
-                          is a set of parameters to reproduce the very
-                          computation. Some transformations may have been
-                          applied to the input kwargs (e.g. xyz files
-                          are read and stored as a list of atoms and
-                          coordinates)
 
   A couple of selected kwargs:
     - basis_type        The type of the basis used for the calculation
     - coords            List of iterables of size 3: Coordinats of the atoms
     - atoms             List of the atom symbols (in the same order as coords)
+    - guess             The guess method to use
   """
+  #
+  # Input
+  #
+  # Keys which need to be parsed *after* all other ones have been.
+  delayed_keys = [ "guess", "guess_external_orben_f", "guess_external_orbcoeff_bf" ]
+
+  inputargs = dict()
+  for key in kwargs:
+    if not key in hartree_fock_keys:
+      raise ValueError("Keyword " + key + " is unknown to hartree_fock")
+    if key in delayed_keys:
+      continue
+
+    # Copy key and (possibly transformed) value:
+    if key in __kwargs_parse_map:
+      __kwargs_parse_map[key](kwargs,inputargs)
+    else:
+      inputargs[key] = kwargs[key]
+
+  # Deal with the delayed keys:
+  for key in delayed_keys:
+    if key in kwargs:
+      __kwargs_parse_map[key](kwargs,inputargs)
+
+  # Setup parameters:
+  params = iface.Parameters()
+  for key in inputargs:
+    if key in __params_transform_map:
+      setattr(params, key, __params_transform_map[key](inputargs[key]))
+    else:
+      setattr(params, key, inputargs[key])
+
+  if "restricted" in kwargs:
+    # Make a note that the user specified the restricted keyword
+    params.internal_restricted_set_by_user = True
+
+  #
+  res = iface.hartree_fock(params)
+  #
+
+  #
+  # Output
+  #
   # TODO Better return a dict-like class instead of a dict. That way
   #      we can use the class more easily and distinguish between results
   #      at different levels better.
-
-  # The list of valid keys is the list of keys
-  # with the special ones (starting with __) removed.
-  params_keys = [ k for k in dir(iface.Parameters) if k[0] != "_" ]
   res_keys = [ k for k in dir(iface.HfResults) if k[0] != "_" ]
-
-  # Build params and run:
-  params = iface.Parameters()
-  for key in kwargs:
-    if not key in params_keys or key in HF_PARAMS_EXCLUDE_KEYS:
-      raise ValueError("Keyword " + key + " is unknown to hartree_fock")
-    elif key in __params_transform_maps:
-      setattr(params,key,__params_transform_maps[key](kwargs[key]))
-    else:
-      setattr(params,key,kwargs[key])
-
-  # Make a note that the user specified the restricted keyword
-  if "restricted" in kwargs: params.restricted_set_by_user = True
-
-  res = iface.hartree_fock(params)
-
-  # Build output dictionary:
   shape_lookup = { "f": res.n_orbs_alpha + res.n_orbs_beta,
                    "b": res.n_bas }
 
@@ -166,8 +241,7 @@ def hartree_fock(forward_parameters=True, **kwargs):
       # so we can ignore it
       out[k] = ary.reshape(target_shape)
 
-  if forward_parameters:
-    out[HFRES_INPUT_PARAMETER_KEY] = kwargs
+  # Forward input parameters to output
+  out[HFRES_INPUT_PARAMETER_KEY] = inputargs
 
   return out
-
---
+++ src/molsturm_iface/molsturm/scf_guess/_impl.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2017 by the molsturm authors
+##
+## This file is part of molsturm.
+##
+## molsturm is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## molsturm is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with molsturm. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+## vi: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+
+from .._sturmian import build_basis_projector, build_nlm_basis
+from .._constants import HFRES_INPUT_PARAMETER_KEY
+import numpy as np
+
+def __crop_orben_orbcoeff(restricted, n_alpha, n_beta, orben, orbcoeff):
+  """
+  Take an orben and an orbcoeff array and crop it in a way to serve as
+  a guess for a system with n_alpha and n_beta electrons which is modelled
+  in restricted or unrestricted manor
+  """
+
+  if restricted:
+    return orben[:n_alpha], orbcoeff[:,:n_alpha]
+  else:
+    n = max(n_alpha, n_beta)
+    n_orbs_alpha = orben.shape[0]//2
+    if 2*n_orbs_alpha != orben.shape[0]:
+      raise ValueError("orben does not have an even number of entries.")
+    return np.concatenate( (orben[:n], orben[n_orbs_alpha:n_orbs_alpha+n]) ), \
+        np.concatenate( (orbcoeff[:,:n], orbcoeff[:, n_orbs_alpha:n_orbs_alpha+n]),
+                       axis=1)
+
+
+def __extrapolate_from_previous_gaussian(old_hfres, kwargs):
+  old_kwargs  = old_hfres[HFRES_INPUT_PARAMETER_KEY]
+  old_orben    = old_hfres["orben_f"]
+  old_orbcoeff = old_hfres["orbcoeff_bf"]
+
+  # TODO Here we assume the n_bas and n_alpha/n_beta values will
+  #      be the same such that we can plainly copy the old results
+  #      over
+  n_elec = max(old_hfres["n_alpha"], old_hfres["n_beta"])
+  return __crop_orben_orbcoeff(old_hfres["restricted"], old_hfres["n_alpha"],
+                               old_hfres["n_beta"], old_orben, old_orbcoeff)
+
+def __extrapolate_from_previous_sturmian(old_hfres, kwargs):
+  old_kwargs  = old_hfres[HFRES_INPUT_PARAMETER_KEY]
+  old_orben    = old_hfres["orben_f"]
+  old_orbcoeff = old_hfres["orbcoeff_bf"]
+
+  # TODO
+  # This is really hackish, since we reimplement the decisions
+  # which go on inside the C++ code right here!
+  #
+  # The way to get around this is to only transfer nlm_basis to the C++
+  # and do all the parsing on the python side, see also Parameters.hh
+  if "nlm_basis" in old_kwargs or "nlm_basis" in kwargs:
+    raise NotImplementedError("Cannot deal with sturmian bases, "
+                              "which contain 'nlm_basis' at the moment")
+
+  def get_nlm_basis(kwargs):
+    # TODO Here we assume nlm order!
+    order="nlm"
+
+    n = kwargs["n_max"]
+    l = kwargs["l_max"] if "l_max" in kwargs else None
+    m = kwargs["m_max"] if "m_max" in kwargs else None
+    return build_nlm_basis(n,l,m,order=order)
+
+  old_nlm_basis = get_nlm_basis(old_kwargs)
+  nlm_basis     = get_nlm_basis(kwargs)
+
+  assert len(old_nlm_basis) == old_hfres["n_bas"], \
+         "Expected number of basis functions differ: n_bas == " + \
+         str(old_hfres["n_bas"]) + " vs. len(old_nlm_basis) == " + \
+         str(len(old_nlm_basis))
+
+  if old_nlm_basis == nlm_basis:
+    return __crop_orben_orbcoeff(old_hfres["restricted"], old_hfres["n_alpha"],
+                                 old_hfres["n_beta"], old_orben, old_orbcoeff)
+  else:
+    proj = build_basis_projector(old_nlm_basis,nlm_basis)
+    return __crop_orben_orbcoeff(old_hfres["restricted"], old_hfres["n_alpha"],
+                                 old_hfres["n_beta"], old_orben,
+                                 np.matmul(proj, old_orbcoeff))
+
+def extrapolate_from_previous(old_hfres, kwargs):
+  """
+  Extrapolate the old SCF results onto the new parameters to build a
+  guess for the new SCF procedure
+  """
+  old_kwargs  = old_hfres[HFRES_INPUT_PARAMETER_KEY]
+
+  is_gaussian = kwargs["basis_type"].startswith("gaussian/")
+  is_sturmian = kwargs["basis_type"].startswith("sturmian/")
+
+  def check_agreement(key):
+    both_have_key = key in old_kwargs and key in kwargs
+    both_without_key = not key in old_kwargs and not key in kwargs
+    if (both_without_key): return
+
+    if not both_have_key or old_kwargs[key] != kwargs[key]:
+      raise ValueError("Cannot extrapolate an SCF guess if the old and new "
+                       "value for '" + key + "' differ.")
+
+  check_agreement("basis_type")
+  if is_gaussian:
+    check_agreement("basis_set")
+    return __extrapolate_from_previous_gaussian(old_hfres, kwargs)
+  elif is_sturmian:
+    return __extrapolate_from_previous_sturmian(old_hfres, kwargs)
+  else:
+    raise ValueError("Did not understand basis_type: '"+basis_type+"'.")
+
