02f11f23eb3c75c4367bb92a6945dba0d328d219
Michael F. Herbst <info@michael-herbst.com>
2017-07-04 16:14:13 +0200

    FCI examples

---
+++ examples/fci/be_cs_fci.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2017 by the molsturm authors
+##
+## This file is part of molsturm.
+##
+## molsturm is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## molsturm is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with molsturm. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+## vi: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+
+from fci_common import run_fci_for
+
+# TODO Reference for experimental ground state energy
+# Note this is not the experimental value, but a guess
+E_guess = -14.66
+#
+run_fci_for("Be", n_roots=2,
+            experimental_ground_state=E_guess,
+            guess="loewdin"
+           )
