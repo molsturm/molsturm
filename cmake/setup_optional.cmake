145835a5d494afed56ac05ab2b80528615201087
Michael F. Herbst <info@michael-herbst.com>
2017-08-05 16:57:41 +0200

    Use config instead of commandline defines

--- cmake/setup_optional.cmake
+++ cmake/setup_optional.cmake
@@ -1,24 +1,24 @@
 # Setup optional dependencies and features
 # alters these things
 #
-# 	MOLSTURM_DEPENDENCIES			everyone needs these libraries
-# 	MOLSTURM_DEPENDENCIES_DEBUG		debug mode needs these extras
-# 	MOLSTURM_DEPENDENCIES_RELEASE		release mode needs these extras
-# 	MOLSTURM_DEPENDENCIES_TEST		tests need these extra libraries
+#       MOLSTURM_DEPENDENCIES			everyone needs these libraries
+#       MOLSTURM_DEPENDENCIES_DEBUG		debug mode needs these extras
+#       MOLSTURM_DEPENDENCIES_RELEASE		release mode needs these extras
+#       MOLSTURM_DEPENDENCIES_TEST		tests need these extra libraries
 #
 #       MOLSTURM_DEFINITIONS			definitions for all compilation
 #       MOLSTURM_DEFINITIONS_DEBUG		definitions for debug mode
 #       MOLSTURM_DEFINITIONS_RELEASE		definitions for release mode
-#       
+#
 
 ####################
 #-- C++ standard --#
 ####################
 if (DRB_HAS_CXX14_SUPPORT)
 	message(STATUS "Detected C++14 support: Setting MOLSTURM_HAVE_CXX14")
-	LIST(APPEND MOLSTURM_DEFINITIONS "MOLSTURM_HAVE_CXX14")
+	set(MOLSTURM_HAVE_CXX14 ON)
 endif()
 if (DRB_HAS_CXX17_SUPPORT)
 	message(STATUS "Detected C++17 support: Setting MOLSTURM_HAVE_CXX17")
-	LIST(APPEND MOLSTURM_DEFINITIONS "MOLSTURM_HAVE_CXX17")
+	set(MOLSTURM_HAVE_CXX17 ON)
 endif()
