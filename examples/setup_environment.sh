57c01646a370ab82d8225cbf4b153bb6010fafa2
Michael F. Herbst <info@michael-herbst.com>
2017-08-07 19:30:32 +0200

    Simplify setup_environment
    Now we can install molsturm properly and hence do no longer need this
    for molsturm by itself.

--- examples/setup_environment.sh
+++ examples/setup_environment.sh
@@ -19,21 +19,7 @@
 ##
 ## ---------------------------------------------------------------------
 
-# The relative path to the build path
-BUILD_PATH="../build"
-
-# ------------------------------------------------
-
-for dir in $PWD/../src/interface/python $PWD/$BUILD_PATH/src/interface/python; do
-	dir=$(readlink -f $dir)
-	if [ ! -d "$dir" ]; then
-		echo "Not a valid path:  $dir" >&2
-		echo "Probably the examples won't work" >&2
-	fi
-	export PYTHONPATH="$PYTHONPATH:$dir"
-done
-
-# ------------------------------------------------
+echo "Searching for third-party packages (adcc and pyscf)"
 
 for file in $PWD/../../adcc/build/src/adcc_iface/adcc_iface.py $PWD/../../adcc/src/adcc_iface/adcc_iface.i; do
 	if [ -f "$file" ]; then
