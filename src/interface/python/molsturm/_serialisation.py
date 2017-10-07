ad1812537ffd332e9a221327890d0198419e1aa3
Michael F. Herbst <info@michael-herbst.com>
2017-10-07 19:56:54 +0200

    Reimplement load/dump hdf5 state

--- src/interface/python/molsturm/_serialisation.py
+++ src/interface/python/molsturm/_serialisation.py
@@ -21,15 +21,15 @@
 ## ---------------------------------------------------------------------
 ## vi: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
 
-from ._iface import Version
 from ._constants import HFRES_ARRAY_KEYS, INPUT_PARAMETER_KEY
-from ._hdf5 import emplace_dict, extract_group, h5py
-
-import numpy as np
 from datetime import datetime
-from io import IOBase
-import yaml
 from distutils.version import StrictVersion
+from ._hdf5 import h5py
+from ._iface import Version
+from . import _hdf5 as hdf5
+from io import IOBase
+import numpy as np
+import yaml
 
 
 def metadata_common():
@@ -159,66 +159,76 @@ def metadata_yaml(stream):
 # HDF5
 #
 def dump_hdf5(hfres, path):
-  """Take a HartreeFock result and dump the data in hdf5 format.
-     Most of the data will be plain text, but the large numpy
-     arrays are stored in binary form.
+    """Take a HartreeFock result and dump the data in hdf5 format.
+       Most of the data will be plain text, but the large numpy
+       arrays are stored in binary form.
 
-     It can later be picked up using load_hdf5.
+       It can later be picked up using load_hdf5.
 
-     path: File path (Note: *not* a stream)
-  """
-  if not isinstance(path,str):
-    raise TypeError("path needs to be a valid path in form of a string")
+       path: File path (Note: *not* a stream)
+    """
+    if not isinstance(path, str):
+        raise TypeError("path needs to be a valid path in form of a string")
 
-  with h5py.File(path, 'w') as h5f:
-    # Split into the dict corresponding to array values (which are gzipped)
-    # and the rest
-    array_dict = { k : hfres[k] for k in hfres if     k in HFRES_ARRAY_KEYS }
-    rest_dict =  { k : hfres[k] for k in hfres if not k in HFRES_ARRAY_KEYS }
+    with h5py.File(path, 'w') as h5f:
+        # Split into the dict corresponding to array values (which are gzipped)
+        # and the rest
+        array_dict = {k: hfres[k] for k in hfres if k in HFRES_ARRAY_KEYS}
+        rest_dict = {k: hfres[k] for k in hfres if k not in HFRES_ARRAY_KEYS}
 
-    emplace_dict(array_dict, h5f, compression="gzip")
-    emplace_dict(rest_dict,  h5f)
+        hdf5.emplace_dict(array_dict, h5f, compression="gzip")
+        hdf5.emplace_dict(rest_dict,  h5f)
 
-    # Write meta data as attributes to root group
-    meta = metadata_common()
-    meta["format_type"] =  "hdf5"
-    meta["format_version"] = "1.0.0"
+        # Write meta data as attributes to root group
+        meta = metadata_common()
+        meta["format_type"] = "hdf5"
+        meta["format_version"] = "0.1.0"
 
-    for attr in meta:
-      h5f.attrs[attr] = meta[attr]
+        for attr in meta:
+            h5f.attrs[attr] = meta[attr]
+
 
 def load_hdf5(path, meta_check=True):
-  """Read an input file or stream in the molsturm hdf5 format
-     and return the parsed hfres dictionary.
+    """Read an input file or stream in the molsturm hdf5 format
+       and return the parsed hfres dictionary.
 
-     Note that ``dump_hdf5(dict,file); dict = load_hdf5(file)``
-     is an identity.
+       Note that ``dump_hdf5(dict,file); dict = load_hdf5(file)``
+       is an identity.
 
-     path:         File path (Note: *not* a stream)
-     meta_check:   If False skips the check of the metadata
-  """
-  parser_max_version = "1.0.0"   # Maximal file format version this parser can do
+       path:         File path (Note: *not* a stream)
+       meta_check:   If False skips the check of the metadata
+    """
+    # Minimal and maximal version this parser understands
+    parser_min_version = "0.1.0"
+    parser_max_version = parser_min_version
 
-  with h5py.File(path,"r") as h5f:
-    if meta_check:
-      # Check metadata first
-      try:
-        meta_type = h5f.attrs["format_type"]
-        meta_version = h5f.attrs["format_version"]
-      except KeyError:
-        raise ValueError("Could not find required metadata from HDF5 file for metadata "
-                         "check. You can try disabling the metadata check using "
-                         "'meta_check=False'. but strange things maght happen ...")
+    def check_metadata(h5f):
+        try:
+            meta_type = h5f.attrs["format_type"]
+            meta_version = h5f.attrs["format_version"]
+        except KeyError:
+            raise ValueError("Could not find required metadata from HDF5 file for "
+                             "metadata check. You can disable checking the metadata "
+                             "using 'meta_check=False', but strange things might "
+                             "happen ...")
 
-      if meta_type != "hdf5":
-        raise ValueError("Hdf5 file format is not 'hdf5', but '"
-                         +str(meta_type)+ "'.")
+        if meta_type != "hdf5":
+            raise ValueError("Hdf5 file format is not 'hdf5', but '" +
+                             str(meta_type) + "'.")
 
-      if StrictVersion(meta_version) > StrictVersion(parser_max_version):
-        raise ValueError("Parser not compatible to versions beyond "
-                         + parser_max_version + ". Encountered version "
-                         + meta_version + ".")
-    return extract_group(h5f)
+        if StrictVersion(meta_version) < StrictVersion(parser_min_version):
+            raise ValueError("Parser not compatible to versions below " +
+                             parser_min_version + ". Encountered version " +
+                             meta_version + ".")
+        elif StrictVersion(meta_version) > StrictVersion(parser_max_version):
+            raise ValueError("Parser not compatible to versions beyond " +
+                             parser_max_version + ". Encountered version " +
+                             meta_version + ".")
+
+    with h5py.File(path, "r") as h5f:
+        if meta_check:
+            check_metadata(h5f)
+        return hdf5.extract_group(h5f)
 
 
 def metadata_hdf5(path):
