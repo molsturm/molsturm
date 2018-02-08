ac49a3b1a8463b12db40b53a8bec427a58fb5f17
Michael F. Herbst <info@michael-herbst.com>
2018-02-08 17:07:39 +0100

    Simplify script

--- examples/integral_interface/restricted_hartree_fock.py
+++ examples/integral_interface/restricted_hartree_fock.py
@@ -1,78 +1,74 @@
 #!/usr/bin/env python3
 
 import molsturm
-import molsturm.sturmian
 from molsturm import integrals
 from scipy import linalg
 import numpy as np
 
-# Run a closed-shell hartree fock using a self-coded
-# routine in python.
+params = molsturm.ScfParameters()
 
+# Supply molecular structure
+atoms = ["Be"]  # as a list of names, symbols, atom numbers ...
+positions = [[0, 0, 0]]  # as a list of triples x,y,z
+system = molsturm.System(atoms, positions)
+system.charge = 0  # Optional, 0 by default
+system.multiplicity = 1  # Optional, 1 by default for even-electron systems,
+#                                    2 for odd-electron systems
+params.system = system
 
-def run_scf(system, basis):
-    params = molsturm.ScfParameters()
-    params.system = system
-    params.basis = basis
+# Supply basis set information
+basis = molsturm.construct_basis("gaussian", system, basis_set_name="pc-3")
+# basis = molsturm.construct_basis("sturmian/atomic", system, k_exp=1.988,
+#                                  n_max=5, l_max=1, m_max=1)
+params.basis = basis
 
-    if system.n_alpha != system.n_beta:
-        raise ValueError("Sorry only closed shell")
-    n_orb = system.n_alpha
+# Compute integral data
+print("Computing integrals ... ", end="", flush=True)
+s_bb = integrals.overlap_bb(params)
+t_bb = integrals.kinetic_bb(params)
+v_bb = integrals.nuclear_attraction_bb(params)
+eri_bbbb = integrals.electron_repulsion_bbbb(params)
+print("done")
 
-    # get integrals in memory
-    print("Computing integrals ... ", end="", flush=True)
-    t_bb = integrals.kinetic_bb(params)
-    v_bb = integrals.nuclear_attraction_bb(params)
-    s_bb = integrals.overlap_bb(params)
-    eri_bbbb = integrals.electron_repulsion_bbbb(params)
-    print("done")
+#
+# Restricted Hartree-Fock routine starts
+#
 
-    # Fock update routine
-    def update_fock(c_bf):
-        return t_bb + v_bb \
-            + 2 * np.einsum("ai,bi,abcd->cd", c_bf, c_bf, eri_bbbb) \
-            - 1 * np.einsum("ai,bi,cbad->cd", c_bf, c_bf, eri_bbbb)
+# The number of occupied orbitals to compute
+n_orb = system.n_alpha
 
-    def update_coefficients(fock):
-        enes, cf_bf = linalg.eigh(fock, s_bb)
-        c_bf = cf_bf[:, :n_orb]
-        return c_bf
+# Form a hcore guess:
+_, c_bf = linalg.eigh(t_bb + v_bb, s_bb, eigvals=(0, n_orb - 1))
 
-    def hf_energy(c_bf):
-        one_elec = np.einsum("ai,bi,ab", c_bf, c_bf, t_bb + v_bb)
-        two_elec = \
-            2 * np.einsum("ai,bi,cj,dj,abcd", c_bf, c_bf, c_bf, c_bf, eri_bbbb) \
-            - np.einsum("ai,bi,cj,dj,cbad", c_bf, c_bf, c_bf, c_bf, eri_bbbb)
-        return 2 * (one_elec + 0.5 * two_elec)
+oldene = 0
+print("iter     etot          echange")
+for i in range(1, 101):
+    # Form new Fock matrix
+    fock = t_bb + v_bb \
+        + 2 * np.einsum("ai,bi,abcd->cd", c_bf, c_bf, eri_bbbb) \
+        - 1 * np.einsum("ai,bi,cbad->cd", c_bf, c_bf, eri_bbbb)
 
-    def form_guess():
-        # Get an hcore guess:
-        _, c_bf = linalg.eigh(t_bb + v_bb, s_bb, eigvals=(0, n_orb - 1))
-        assert c_bf.shape == (basis.size, n_orb)
-        return c_bf
+    # Diagonalise it:
+    enes, cf_bf = linalg.eigh(fock, s_bb)
 
-    c_bf = form_guess()
-    oldene = 0
-    print("iter     etot          echange")
-    for i in range(1, 101):
-        fock = update_fock(c_bf)
-        c_bf = update_coefficients(fock)
+    # Get occupied coefficients according to Aufbau principle
+    c_bf = cf_bf[:, :n_orb]
 
-        # Convergence display and check
-        ene = hf_energy(c_bf)
-        change = ene - oldene
-        print(i, "  ", ene, "  ", change)
-        oldene = ene
-        if abs(change) < 1e-6:
-            break
+    # Compute new HF energy
+    ene_one_elec = np.einsum("ai,bi,ab", c_bf, c_bf, t_bb + v_bb)
+    ene_two_elec = \
+        2 * np.einsum("ai,bi,cj,dj,abcd", c_bf, c_bf, c_bf, c_bf, eri_bbbb) \
+        - np.einsum("ai,bi,cj,dj,cbad", c_bf, c_bf, c_bf, c_bf, eri_bbbb)
+    ene = 2 * (ene_one_elec + 0.5 * ene_two_elec)
 
+    # Display current iteration
+    ene_change = ene - oldene
+    print(i, "  ", ene, "  ", ene_change)
 
-def main():
-    system = molsturm.System(["Be"])
-    kopt = molsturm.sturmian.cs.empirical_kopt(system)
-    basis = molsturm.construct_basis("gaussian", system, basis_set_name="pc-3")
-    run_scf(system, basis)
+    # Check for convergence
+    if abs(ene_change) < 1e-6:
+        break
 
-
-if __name__ == "__main__":
-    main()
+    # Copy old energy
+    oldene = ene
+# end for
